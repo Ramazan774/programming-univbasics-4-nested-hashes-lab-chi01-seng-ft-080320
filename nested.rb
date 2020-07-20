f66d06fba9af917ed177a83e141c3ddf248b6f97
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-20 19:46:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,14 @@ def hopper
         :languages => ["C"]
       }
     }
-    programmer_hash => {
+end
+ programmer_hash = {
       :known_for => "COBOL",
       :languages => "COBOL", "FORTRAN"
     }
-
-end
-
+    
+    puts programmer_hash
+    
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
