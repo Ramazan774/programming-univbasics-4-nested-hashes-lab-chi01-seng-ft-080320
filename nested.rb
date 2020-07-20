8795ac664fb6ff8923a71ab87fe5dba9991e1bd3
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-20 20:48:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -151,7 +151,7 @@ def adding_to_dennis
     
     programmer_hash = {
       :dennis_ritchie => {
-        :languages => ["Assembly"]
+        :languages => ["Smalltalk", "LISP","Assembly"]
       }
     }
     return programmer_hash
