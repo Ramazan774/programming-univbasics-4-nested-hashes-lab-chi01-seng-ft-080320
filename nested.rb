7e2aba5ec413fb6fcde4c44af7d89d5e46c20b2a
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-20 20:30:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
       }
     }
    
-  programmer_hash [:known_for] = "COBOL"
+  programmer_hash => [:known_for] = "COBOL"
   programmer_hash [:languages] = ["COBOL", "FORTRAN"]
   
     return programmer_hash
