f86bc1d34e12ad89afdde04e9adcc3e317c68668
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-20 20:13:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,12 +14,11 @@ def hopper
         :languages => ["C"]
       }
     }
-  end
     
     programmer_hash[:grace_hopper] = programmer_hash[:grace_hopper].split()
     
     return programmer_hash
-
+end
    
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
